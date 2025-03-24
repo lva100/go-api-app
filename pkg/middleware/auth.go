81c9bbab22d3641825e9ac49455315de5233be7d
lva100 <lva100@yandex.ru>
2025-03-24 17:21:13 +0300

    read and write Context

--- pkg/middleware/auth.go
+++ pkg/middleware/auth.go
@@ -1,21 +1,29 @@
 package middleware
 
 import (
-	"fmt"
+	"context"
 	"go/adv-demo/configs"
 	"go/adv-demo/pkg/jwt"
 	"net/http"
 	"strings"
 )
 
+type key string
+
+const (
+	ContextEmailKey key = "ContextEmailKey"
+)
+
 func IsAuthed(next http.Handler, config *configs.Config) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		authHeader := r.Header.Get("Authorization")
 		token := strings.TrimPrefix(authHeader, "Bearer ")
-		isValid, data := jwt.NewJWT(config.Auth.Secret).Parse(token)
-		fmt.Println("TOKEN -", token)
-		fmt.Println("ISVALID -", isValid)
-		fmt.Println("DATA -", data)
-		next.ServeHTTP(w, r)
+		_, data := jwt.NewJWT(config.Auth.Secret).Parse(token)
+		ctx := context.WithValue(r.Context(), ContextEmailKey, data.Email)
+		req := r.WithContext(ctx)
+		// fmt.Println("TOKEN -", token)
+		// fmt.Println("ISVALID -", isValid)
+		// fmt.Println("DATA -", data)
+		next.ServeHTTP(w, req)
 	})
 }
