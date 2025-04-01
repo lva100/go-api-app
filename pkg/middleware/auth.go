061d5a8b6d58ac88d45b5c6e51d0412629e12058
lva100 <lva100@yandex.ru>
2025-04-01 10:53:42 +0300

    return message unauthed

--- pkg/middleware/auth.go
+++ pkg/middleware/auth.go
@@ -14,11 +14,24 @@ const (
 	ContextEmailKey key = "ContextEmailKey"
 )
 
+func writeUnauthed(w http.ResponseWriter) {
+	w.WriteHeader(http.StatusUnauthorized)
+	w.Write([]byte(http.StatusText(http.StatusUnauthorized)))
+}
+
 func IsAuthed(next http.Handler, config *configs.Config) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		authHeader := r.Header.Get("Authorization")
+		if !strings.HasPrefix(authHeader, "Bearer ") {
+			writeUnauthed(w)
+			return
+		}
 		token := strings.TrimPrefix(authHeader, "Bearer ")
-		_, data := jwt.NewJWT(config.Auth.Secret).Parse(token)
+		isValid, data := jwt.NewJWT(config.Auth.Secret).Parse(token)
+		if !isValid {
+			writeUnauthed(w)
+			return
+		}
 		ctx := context.WithValue(r.Context(), ContextEmailKey, data.Email)
 		req := r.WithContext(ctx)
 		// fmt.Println("TOKEN -", token)
