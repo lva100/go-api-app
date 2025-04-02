52b062562b8e8397322446678abbdf32efc08755
lva100 <lva100@yandex.ru>
2025-04-02 15:43:38 +0300

    Prepare data for testing env

--- cmd/auth_test.go
+++ cmd/auth_test.go
@@ -4,20 +4,57 @@ import (
 	"bytes"
 	"encoding/json"
 	"go/adv-demo/internal/auth"
+	"go/adv-demo/internal/user"
 	"go/adv-demo/pkg/hsrv"
 	"io"
 	"net/http"
 	"net/http/httptest"
+	"os"
 	"testing"
+
+	"github.com/joho/godotenv"
+	"golang.org/x/crypto/bcrypt"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
 )
 
+func initDB() *gorm.DB {
+	err := godotenv.Load(".env")
+	if err != nil {
+		panic(err)
+	}
+	db, err := gorm.Open(postgres.Open(os.Getenv("DSN")), &gorm.Config{})
+	if err != nil {
+		panic(err)
+	}
+	return db
+}
+
+func initData(db *gorm.DB) {
+	hashedPassword, _ := bcrypt.GenerateFromPassword([]byte("1"), bcrypt.DefaultCost)
+	db.Create(&user.User{
+		Email:    "test@test.ru",
+		Password: string(hashedPassword),
+		Name:     "Test user",
+	})
+}
+
+func removeData(db *gorm.DB) {
+	db.Unscoped().
+		Where("email = ?", "test@test.ru").
+		Delete(&user.User{})
+}
+
 func TestLoginAccess(t *testing.T) {
+	//Prepare data
+	db := initDB()
+	initData(db)
 	ts := httptest.NewServer(hsrv.App())
 	defer ts.Close()
 
 	data, _ := json.Marshal(&auth.LoginRequest{
-		Email:    "vasya@mail.ru",
-		Password: "123",
+		Email:    "test@test.ru",
+		Password: "1",
 	})
 	res, err := http.Post(ts.URL+"/auth/login", "application/json", bytes.NewReader(data))
 	if err != nil {
@@ -38,9 +75,14 @@ func TestLoginAccess(t *testing.T) {
 	if resData.Token == "" {
 		t.Fatal("Token is empty")
 	}
+	removeData(db)
 }
 
 func TestLoginFail(t *testing.T) {
+	//Prepare data
+	db := initDB()
+	initData(db)
+
 	ts := httptest.NewServer(hsrv.App())
 	defer ts.Close()
 
@@ -55,4 +97,5 @@ func TestLoginFail(t *testing.T) {
 	if res.StatusCode != 401 {
 		t.Fatalf("Expected %d got %d", 401, res.StatusCode)
 	}
+	removeData(db)
 }
